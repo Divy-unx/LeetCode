b9b1b42870ab492fe156e3f01e99439d122e3523
Divyanshu Mishra <divyanshum635@gmail.com>
2026-08-13 21:50:28 +0530

    Time: 2 ms (27.66%), Space: 45.2 MB (81.23%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder s = new StringBuilder();
+        s.append(chars[0]);
+        int count = 1;
+
+        for(int i = 1 ; i < chars.length; i++){
+            char curr = chars[i];
+            char prev = chars[i-1];
+
+            if(curr == prev){
+                count++;
+            }else{
+                if(count > 1){
+                    s.append(count);
+                }
+                s.append(curr);
+                count = 1;
+            }
+        }
+
+        if(count > 1){
+            s.append(count);
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            chars[i] = s.charAt(i);
+        }
+
+        return s.length();
+    }
+}
